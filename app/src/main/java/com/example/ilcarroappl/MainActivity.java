615a36abb80416853afd2def41b433659205e6f2
Mikhail Hursky <taraka_39@mail.ru>
2020-02-27 03:02:15 +0300

    toolBar + MainAct

--- app/src/main/java/com/example/ilcarroappl/MainActivity.java
+++ app/src/main/java/com/example/ilcarroappl/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.ilcarroappl;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.jar.Attributes;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MY_TAG";
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    MenuItem searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        searchItem = findViewById(R.id.search);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.search) {
+            Toast.makeText(this, "Search logo", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer((GravityCompat.START));
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.close_nav_btn:
+                drawerLayout.closeDrawer((GravityCompat.START));
+                break;
+            case R.id.nav_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_favorites:
+                Toast.makeText(this, "Favorites", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_login:
+                Toast.makeText(this, "Login", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_sign_up:
+                Toast.makeText(this, "Sign up", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_terms_of_use:
+                Toast.makeText(this, "Terms of use", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_work:
+                Toast.makeText(this, "Work", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
     }
 }
+
