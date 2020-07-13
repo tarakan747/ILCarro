fa3399ff7273454dfb2292ef82dcf456063ae3d5
tarakan747 <55385692+tarakan747@users.noreply.github.com>
2020-07-13 22:41:48 +0300

    Delete HttpProvider.java

--- app/src/main/java/com/example/ilcarroappl/HttpProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.ilcarroappl;
-import java.io.IOException;
-
-
-public interface HttpProvider {
-    UserDto registration(String email, String password, String firstName, String lastName) throws IOException;
-}
